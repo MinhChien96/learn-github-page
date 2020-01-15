63dbf8782ef781b7b58bda96df407d20c72b079e
Chien Nguyen Minh <chiennm@nissho-vn.com>
2020-01-15 13:41:15 +0630

    Updates

---
+++ precache-manifest.7b4bfeb9c27d52a2dfaeb122ad2f68ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8ccab53e2827b4ad022c6472a66b7ea",
+    "url": "/learn-github-page/index.html"
+  },
+  {
+    "revision": "c8bf80cec2bd0f6df92c",
+    "url": "/learn-github-page/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8dec8e9bee382013305f",
+    "url": "/learn-github-page/static/js/2.a9f7297b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/learn-github-page/static/js/2.a9f7297b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8bf80cec2bd0f6df92c",
+    "url": "/learn-github-page/static/js/main.344acb82.chunk.js"
+  },
+  {
+    "revision": "e2d81e0a4341b298d560",
+    "url": "/learn-github-page/static/js/runtime-main.c3afba2b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/learn-github-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
